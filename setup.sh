638c1c5422d5df02aaec802958050e79529d8080
saprative <saprative@gmail.com>
2024-01-28 21:57:59 +0530

    updated dotfiles

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Symbolink files
-ln -sf ~/dotfiles/.vimrc ~/
-ln -sf ~/dotfiles/.tmux.conf ~/
-ln -sf ~/dotfiles/.emacs ~/
-ln -sf ~/dotfiles/.gitconfig ~/
-ln -sf ~/dotfiles/.bash_profile ~/
-ln -sf ~/dotfiles/.bashrc ~/
+ln -sf ~/.dotfiles/.vimrc ~/
+ln -sf ~/.dotfiles/.tmux.conf ~/
+ln -sf ~/.dotfiles/.emacs ~/
+ln -sf ~/.dotfiles/.gitconfig ~/
+ln -sfr ~/.dotfiles/nvim ~/.config/
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
 # Promt
-echo "Setup: Vim, Git, Emacs, Tmux"
+echo "Congratulations!! 
+    1. Vim
+    2. NeoVim
+    3. Git
+    4. Emacs
+    5. Tmux
+    6. Oh My Zsh 
+Setup is Successful."
