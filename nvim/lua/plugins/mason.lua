72d2768f9d0aecbfcfdf0c0791ee258e9248a86b
saprative <saprative@gmail.com>
2025-11-11 04:38:57 +0000

    added gemini

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"jedi_language_server",
+				"tailwindcss",
+				"eslint",
+				"phpactor",
+				"ts_ls",
+				"vtsls",
+				"emmet_language_server",
+			},
+		})
+	end,
+}
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -11,10 +11,15 @@ return {
 		filesystem = {
 			filtered_items = {
 				visible = false, -- when true, they will just be displayed differently than normal items
-				hide_dotfiles = true,
+				hide_dotfile = true,
 				hide_gitignored = true,
 				hide_hidden = true, -- only works on Windows for hidden files/directories
 			},
 		},
 	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
+		vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>")
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+	end,
 }
